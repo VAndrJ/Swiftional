e45e65eec8f0487e3ea6c09cc350a0cae2a99285
Volodymyr Andriienko <volodymyr.andriienko@wefuture.co.uk>
2023-05-01 18:13:15 +0300

    Remove unused

--- Sources/Swiftional/HKT.swift
+++ Sources/Swiftional/HKT.swift
@@ -17,20 +17,7 @@ import Foundation
 ///
 /// Type parameter `F` is reserved for a witness to prevent circular references in the inheritance relationship.
 /// By convention, witnesses are named like the class they represent, with the prefix `For`.
-public class HKT<F, A> {
-
-    public init() {}
-}
-
-/// Higher-Kinded Type with 2 type arguments.
-///
-/// This class simulates Higher-Kinded Type support. `HKT<F, A, B>` is an alias for `F<A, B>`, which is not syntactically valid. So, instead of writing `F<A, B>`, we will write `HKT<F, A, B>`.
-/// This is useful because in the expression `HKT<F, A, B>`, both `F`, `B` and `A` have kind `*`,
-/// so we can work with them using the type system we currently have at our disposal.
-///
-/// Type parameter `F` is reserved for a witness to prevent circular references in the inheritance relationship.
-/// By convention, witnesses are named like the class they represent, with the prefix `For`.
-public class HKT2<F, A> {
+open class HKT<F, A> {
 
     public init() {}
 }
