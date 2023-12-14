cfd0a9d45ac3ce1d6ab64f2b894c3253a56ed605
Volodymyr Andriienko <vandrjios@gmail.com>
2023-12-14 09:53:57 +0200

    Async curry.

--- Sources/Swiftional/Curry.swift
+++ Sources/Swiftional/Curry.swift
@@ -15,12 +15,64 @@ public func curry<A, B, R>(_ fn: @escaping (A, B) -> R) -> (A) -> (B) -> R {
     { a in { b in fn(a, b) } }
 }
 
+/// Converts an uncurried throws function with 2 arguments to a curried throws function.
+///
+/// - Parameter fn: Function to be curried.
+/// - Returns: Curried form of an argument function `fn`: (A) -> (B) -> R
+public func curry<A, B, R>(_ fn: @escaping (A, B) throws -> R) -> (A) -> (B) throws -> R {
+    { a in { b in try fn(a, b) } }
+}
+
+/// Converts an uncurried async function with 2 arguments to a curried async function.
+///
+/// - Parameter fn: Function to be curried.
+/// - Returns: Curried form of an argument function `fn`: (A) -> (B) -> R
+@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+public func curry<A, B, R>(_ fn: @escaping @Sendable (A, B) async -> R) -> @Sendable (A) -> @Sendable (B) async -> R {
+    { a in { b in await fn(a, b) } }
+}
+
+/// Converts an uncurried async throws function with 2 arguments to a curried async throws function.
+///
+/// - Parameter fn: Function to be curried.
+/// - Returns: Curried form of an argument function `fn`: (A) -> (B) -> R
+@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+public func curry<A, B, R>(_ fn: @escaping @Sendable (A, B) async throws -> R) -> @Sendable (A) -> @Sendable (B) async throws -> R {
+    { a in { b in try await fn(a, b) } }
+}
+
 /// Converts an uncurried function with 3 arguments to a curried function.
 ///
 /// - Parameter fn: Function to be curried.
 /// - Returns: Curried form of an argument function `fn`: (A) -> (B) -> (C) -> R
 public func curry<A, B, C, R>(_ fn: @escaping (A, B, C) -> R) -> (A) -> (B) -> (C) -> R {
     { a in { b in { c in fn(a, b, c) } } }
+}
+
+/// Converts an uncurried throws function with 3 arguments to a curried throws function.
+///
+/// - Parameter fn: Function to be curried.
+/// - Returns: Curried form of an argument function `fn`: (A) -> (B) -> (C) -> R
+public func curry<A, B, C, R>(_ fn: @escaping (A, B, C) throws -> R) -> (A) -> (B) -> (C) throws -> R {
+    { a in { b in { c in try fn(a, b, c) } } }
+}
+
+/// Converts an uncurried async function with 3 arguments to a curried async function.
+///
+/// - Parameter fn: Function to be curried.
+/// - Returns: Curried form of an argument function `fn`: (A) -> (B) -> (C) -> R
+@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+public func curry<A, B, C, R>(_ fn: @escaping @Sendable (A, B, C) async -> R) -> (A) -> (B) -> (C) async -> R {
+    { a in { b in { c in await fn(a, b, c) } } }
+}
+
+/// Converts an uncurried async throws function with 3 arguments to a curried async throws function.
+///
+/// - Parameter fn: Function to be curried.
+/// - Returns: Curried form of an argument function `fn`: (A) -> (B) -> (C) -> R
+@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+public func curry<A, B, C, R>(_ fn: @escaping @Sendable (A, B, C) async throws -> R) -> @Sendable (A) -> @Sendable (B) -> @Sendable (C) async throws -> R {
+    { a in { b in { c in try await fn(a, b, c) } } }
 }
 
 /// Converts an uncurried function with 4 arguments to a curried function.
@@ -31,6 +83,32 @@ public func curry<A, B, C, D, R>(_ fn: @escaping (A, B, C, D) -> R) -> (A) -> (B
     { a in { b in { c in { d in fn(a, b, c, d) } } } }
 }
 
+/// Converts an uncurried throws function with 4 arguments to a curried throws function.
+///
+/// - Parameter fn: Function to be curried.
+/// - Returns: Curried form of an argument function `fn`: (A) -> (B) -> (C) -> (D) -> R
+public func curry<A, B, C, D, R>(_ fn: @escaping (A, B, C, D) throws -> R) -> (A) -> (B) -> (C) -> (D) throws -> R {
+    { a in { b in { c in { d in try fn(a, b, c, d) } } } }
+}
+
+/// Converts an uncurried async function with 4 arguments to a curried async function.
+///
+/// - Parameter fn: Function to be curried.
+/// - Returns: Curried form of an argument function `fn`: (A) -> (B) -> (C) -> (D) -> R
+@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+public func curry<A, B, C, D, R>(_ fn: @escaping @Sendable (A, B, C, D) async -> R) -> @Sendable (A) -> @Sendable (B) -> @Sendable (C) -> @Sendable (D) async -> R {
+    { a in { b in { c in { d in await fn(a, b, c, d) } } } }
+}
+
+/// Converts an uncurried async throws function with 4 arguments to a curried async throws function.
+///
+/// - Parameter fn: Function to be curried.
+/// - Returns: Curried form of an argument function `fn`: (A) -> (B) -> (C) -> (D) -> R
+@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+public func curry<A, B, C, D, R>(_ fn: @escaping @Sendable (A, B, C, D) async throws -> R) -> @Sendable (A) -> @Sendable (B) -> @Sendable (C) -> @Sendable (D) async throws -> R {
+    { a in { b in { c in { d in try await fn(a, b, c, d) } } } }
+}
+
 /// Converts an uncurried function with 5 arguments to a curried function.
 ///
 /// - Parameter fn: Function to be curried.
@@ -39,10 +117,62 @@ public func curry<A, B, C, D, E, R>(_ fn: @escaping (A, B, C, D, E) -> R) -> (A)
     { a in { b in { c in { d in { e in fn(a, b, c, d, e) } } } } }
 }
 
+/// Converts an uncurried throws function with 5 arguments to a curried throws function.
+///
+/// - Parameter fn: Function to be curried.
+/// - Returns: Curried form of an argument function `fn`: (A) -> (B) -> (C) -> (D) -> (E) -> R
+public func curry<A, B, C, D, E, R>(_ fn: @escaping (A, B, C, D, E) throws -> R) -> (A) -> (B) -> (C) -> (D) -> (E) throws -> R {
+    { a in { b in { c in { d in { e in try fn(a, b, c, d, e) } } } } }
+}
+
+/// Converts an uncurried async function with 5 arguments to a curried async function.
+///
+/// - Parameter fn: Function to be curried.
+/// - Returns: Curried form of an argument function `fn`: (A) -> (B) -> (C) -> (D) -> (E) -> R
+@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+public func curry<A, B, C, D, E, R>(_ fn: @escaping @Sendable (A, B, C, D, E) async -> R) -> @Sendable (A) -> @Sendable (B) -> @Sendable (C) -> @Sendable (D) -> @Sendable (E) async -> R {
+    { a in { b in { c in { d in { e in await fn(a, b, c, d, e) } } } } }
+}
+
+/// Converts an uncurried async throws function with 5 arguments to a curried async throws function.
+///
+/// - Parameter fn: Function to be curried.
+/// - Returns: Curried form of an argument function `fn`: (A) -> (B) -> (C) -> (D) -> (E) -> R
+@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+public func curry<A, B, C, D, E, R>(_ fn: @escaping @Sendable (A, B, C, D, E) async throws -> R) -> @Sendable (A) -> @Sendable (B) -> @Sendable (C) -> @Sendable (D) -> @Sendable (E) async throws -> R {
+    { a in { b in { c in { d in { e in try await fn(a, b, c, d, e) } } } } }
+}
+
 /// Converts an uncurried function with 6 arguments to a curried function.
 ///
 /// - Parameter fn: Function to be curried.
 /// - Returns: Curried form of an argument function `fn`: (A) -> (B) -> (C) -> (D) -> (E) -> (F) -> R
 public func curry<A, B, C, D, E, F, R>(_ fn: @escaping (A, B, C, D, E, F) -> R) -> (A) -> (B) -> (C) -> (D) -> (E) -> (F) -> R {
     { a in { b in { c in { d in { e in { f in fn(a, b, c, d, e, f) } } } } } }
+}
+
+/// Converts an uncurried throws function with 6 arguments to a curried throws function.
+///
+/// - Parameter fn: Function to be curried.
+/// - Returns: Curried form of an argument function `fn`: (A) -> (B) -> (C) -> (D) -> (E) -> (F) -> R
+public func curry<A, B, C, D, E, F, R>(_ fn: @escaping (A, B, C, D, E, F) throws -> R) -> (A) -> (B) -> (C) -> (D) -> (E) -> (F) throws -> R {
+    { a in { b in { c in { d in { e in { f in try fn(a, b, c, d, e, f) } } } } } }
+}
+
+/// Converts an uncurried async function with 6 arguments to a curried async function.
+///
+/// - Parameter fn: Function to be curried.
+/// - Returns: Curried form of an argument function `fn`: (A) -> (B) -> (C) -> (D) -> (E) -> (F) -> R
+@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+public func curry<A, B, C, D, E, F, R>(_ fn: @escaping @Sendable (A, B, C, D, E, F) async -> R) -> @Sendable (A) -> @Sendable (B) -> @Sendable (C) -> @Sendable (D) -> @Sendable (E) -> @Sendable (F) async -> R {
+    { a in { b in { c in { d in { e in { f in await fn(a, b, c, d, e, f) } } } } } }
+}
+
+/// Converts an uncurried async throws function with 6 arguments to a curried async throws function.
+///
+/// - Parameter fn: Function to be curried.
+/// - Returns: Curried form of an argument function `fn`: (A) -> (B) -> (C) -> (D) -> (E) -> (F) -> R
+@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+public func curry<A, B, C, D, E, F, R>(_ fn: @escaping @Sendable (A, B, C, D, E, F) async throws -> R) -> @Sendable (A) -> @Sendable (B) -> @Sendable (C) -> @Sendable (D) -> @Sendable (E) -> @Sendable (F) async throws -> R {
+    { a in { b in { c in { d in { e in { f in try await fn(a, b, c, d, e, f) } } } } } }
 }
--- Tests/SwiftionalTests/CurryTests.swift
+++ Tests/SwiftionalTests/CurryTests.swift
@@ -16,10 +16,54 @@ class CurryTests: XCTestCase {
         XCTAssertEqual(expected, curry(f2)(arr[0])(arr[1]))
     }
 
+    func test_throws_curryingTwoArgumentsFunction_sameResults() throws {
+        let arr = generateTestIntArray(length: 2)
+        let expected = try f2t(arr[0], arr[1])
+        XCTAssertEqual(expected, try curry(f2t)(arr[0])(arr[1]))
+    }
+
+    func test_async_curryingTwoArgumentsFunction_sameResults() async {
+        let arr = generateTestIntArray(length: 2)
+        let expected = await aF2(arr[0], arr[1])
+        let sut = curry(aF2)
+        let result = await sut(arr[0])(arr[1])
+        XCTAssertEqual(expected, result)
+    }
+
+    func test_async_throws_curryingTwoArgumentsFunction_sameResults() async throws {
+        let arr = generateTestIntArray(length: 2)
+        let expected = try await aF2T(arr[0], arr[1])
+        let sut = curry(aF2T)
+        let result = try await sut(arr[0])(arr[1])
+        XCTAssertEqual(expected, result)
+    }
+
     func test_curryingThreeArgumentsFunction_sameResults() {
         let arr = generateTestIntArray(length: 3)
         let expected = f3(arr[0], arr[1], arr[2])
         XCTAssertEqual(expected, curry(f3)(arr[0])(arr[1])(arr[2]))
+    }
+
+    func test_throws_curryingThreeArgumentsFunction_sameResults() throws {
+        let arr = generateTestIntArray(length: 3)
+        let expected = try f3t(arr[0], arr[1], arr[2])
+        XCTAssertEqual(expected, try curry(f3t)(arr[0])(arr[1])(arr[2]))
+    }
+
+    func test_async_curryingThreeArgumentsFunction_sameResults() async {
+        let arr = generateTestIntArray(length: 3)
+        let expected = await aF3(arr[0], arr[1], arr[2])
+        let sut = curry(aF3)
+        let result = await sut(arr[0])(arr[1])(arr[2])
+        XCTAssertEqual(expected, result)
+    }
+
+    func test_async_throws_curryingThreeArgumentsFunction_sameResults() async throws {
+        let arr = generateTestIntArray(length: 3)
+        let expected = try await aF3T(arr[0], arr[1], arr[2])
+        let sut = curry(aF3T)
+        let result = try await sut(arr[0])(arr[1])(arr[2])
+        XCTAssertEqual(expected, result)
     }
 
     func test_curryingFourArgumentsFunction_sameResults() {
@@ -28,15 +72,81 @@ class CurryTests: XCTestCase {
         XCTAssertEqual(expected, curry(f4)(arr[0])(arr[1])(arr[2])(arr[3]))
     }
 
+    func test_throws_curryingFourArgumentsFunction_sameResults() throws {
+        let arr = generateTestIntArray(length: 4)
+        let expected = try f4t(arr[0], arr[1], arr[2], arr[3])
+        XCTAssertEqual(expected, try curry(f4t)(arr[0])(arr[1])(arr[2])(arr[3]))
+    }
+
+    func test_async_curryingFourArgumentsFunction_sameResults() async {
+        let arr = generateTestIntArray(length: 4)
+        let expected = await aF4(arr[0], arr[1], arr[2], arr[3])
+        let sut = curry(aF4)
+        let result = await sut(arr[0])(arr[1])(arr[2])(arr[3])
+        XCTAssertEqual(expected, result)
+    }
+
+    func test_async_throws_curryingFourArgumentsFunction_sameResults() async throws {
+        let arr = generateTestIntArray(length: 4)
+        let expected = try await aF4T(arr[0], arr[1], arr[2], arr[3])
+        let sut = curry(aF4T)
+        let result = try await sut(arr[0])(arr[1])(arr[2])(arr[3])
+        XCTAssertEqual(expected, result)
+    }
+
     func test_curryingFiveArgumentsFunction_sameResults() {
         let arr = generateTestIntArray(length: 5)
         let expected = f5(arr[0], arr[1], arr[2], arr[3], arr[4])
         XCTAssertEqual(expected, curry(f5)(arr[0])(arr[1])(arr[2])(arr[3])(arr[4]))
     }
 
+    func test_throws_curryingFiveArgumentsFunction_sameResults() throws {
+        let arr = generateTestIntArray(length: 5)
+        let expected = try f5t(arr[0], arr[1], arr[2], arr[3], arr[4])
+        XCTAssertEqual(expected, try curry(f5t)(arr[0])(arr[1])(arr[2])(arr[3])(arr[4]))
+    }
+
+    func test_async_curryingFiveArgumentsFunction_sameResults() async {
+        let arr = generateTestIntArray(length: 5)
+        let expected = await aF5(arr[0], arr[1], arr[2], arr[3], arr[4])
+        let sut = curry(aF5)
+        let result = await sut(arr[0])(arr[1])(arr[2])(arr[3])(arr[4])
+        XCTAssertEqual(expected, result)
+    }
+
+    func test_async_throws_curryingFiveArgumentsFunction_sameResults() async throws {
+        let arr = generateTestIntArray(length: 5)
+        let expected = try await aF5T(arr[0], arr[1], arr[2], arr[3], arr[4])
+        let sut = curry(aF5T)
+        let result = try await sut(arr[0])(arr[1])(arr[2])(arr[3])(arr[4])
+        XCTAssertEqual(expected, result)
+    }
+
     func test_curryingSixArgumentsFunction_sameResults() {
         let arr = generateTestIntArray(length: 6)
         let expected = f6(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5])
         XCTAssertEqual(expected, curry(f6)(arr[0])(arr[1])(arr[2])(arr[3])(arr[4])(arr[5]))
+    }
+
+    func test_throws_curryingSixArgumentsFunction_sameResults() throws {
+        let arr = generateTestIntArray(length: 6)
+        let expected = try f6T(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5])
+        XCTAssertEqual(expected, try curry(f6T)(arr[0])(arr[1])(arr[2])(arr[3])(arr[4])(arr[5]))
+    }
+
+    func test_async_curryingSixArgumentsFunction_sameResults() async {
+        let arr = generateTestIntArray(length: 6)
+        let expected = await aF6(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5])
+        let sut = curry(aF6)
+        let result = await sut(arr[0])(arr[1])(arr[2])(arr[3])(arr[4])(arr[5])
+        XCTAssertEqual(expected, result)
+    }
+
+    func test_async_throws_curryingSixArgumentsFunction_sameResults() async throws {
+        let arr = generateTestIntArray(length: 6)
+        let expected = try await aF6T(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5])
+        let sut = curry(aF6T)
+        let result = try await sut(arr[0])(arr[1])(arr[2])(arr[3])(arr[4])(arr[5])
+        XCTAssertEqual(expected, result)
     }
 }
